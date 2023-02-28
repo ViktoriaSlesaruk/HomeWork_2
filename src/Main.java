d9c899d83db10789fc545a9d48340d213b41e181
Viktoryia Slesaruk <viktoryia.slesaruk@indev.by>
2023-02-28 12:32:26 +0300

    First commit

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,30 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Viktoria \nSlesaruk \nQA \nYoga");
-    }
 
+        String correctpass = "Vika";
+
+        while (true) {
+            System.out.println("Введите пароль");
+            Scanner scanner = new Scanner(System.in);
+            String inputpass = scanner.nextLine();
+
+            if (inputpass.equals("Exit"))
+            {
+                break;
+            }
+            else if(!inputpass.equals(correctpass)) {
+                System.out.println("Неверный пароль, попробуйте еще раз");
+            }
+            else if (inputpass.equals(correctpass))
+             {
+                System.out.println("Вы вошли в систему");
+                break;
+            }
+             else {
+                 break;
+            }
+        }
+    }
 }
